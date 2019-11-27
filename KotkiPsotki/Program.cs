9325a8cfc1d2bfb73771bc7f21d12fc059796fc8
KotkiPsotki <agnieszka--borowiec@wp.pl>
2019-11-27 19:26:44 +0100

    Added Program.cs to give the opportunity to start working on the project

---
+++ KotkiPsotki/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KotkiPsotki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+//initial code
+        }
+    }
+}
