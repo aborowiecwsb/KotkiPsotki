35ed5edf6de340583eb9f3944c5fc1b6aec0c3fd
Kwadivald <wiktorkopinski@gmail.com>
2019-11-27 03:20:40 +0100

    Updated gitignore file

---
+++ Wordreader/Wordreader/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Wordreader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("1. Pobierz plik z internetu.");
+                Console.WriteLine("2. Zlicz liczbę liter w pobranym pliku.");
+                Console.WriteLine("3. Zlicz liczbę wyrazów w pliku.");
+                Console.WriteLine("4. Zlicz liczbę znaków interpunkcyjnych w pliku.");
+                Console.WriteLine("5. Zlicz liczbę zdań w pliku.");
+                Console.WriteLine("6. Wygeneruj raport o użyciu liter(A - Z).");
+                Console.WriteLine("7. Zapisz statystyki z punktów 2 - 5 do pliku statystyki.txt.");
+                Console.WriteLine("8. Wyjście z programu.");
+                int menuOption = Convert.ToInt32(Console.ReadLine());
+                if (menuOption == 8)
+                    break;
+            }
+        }
+    }
+}
